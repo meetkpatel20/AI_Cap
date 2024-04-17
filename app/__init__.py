3ee7141576c8f9198361268bffeae2f7679fee00
Adam Lear <adamlear12@gmail.com>
2024-04-17 11:48:34 -0500

    add more features to end game

--- app/__init__.py
+++ app/__init__.py
@@ -5,5 +5,4 @@ from flask import Flask
 
 app = Flask(__name__)
 app.config["TEMPLATES_AUTO_RELOAD"] = True
-app.debug = True
 app.secret_key = "secret key"
--- app/parsing.py
+++ app/parsing.py
@@ -14,7 +14,6 @@ def choice_parsing(choice):
 
 
 def parse(response):
-    """ """
     text = response.split("###")
     title, body, choices = text[1], text[2], text[4]
 
@@ -29,3 +28,41 @@ def parse(response):
         choices[i] = choice_parsing(choices[i])
 
     return (title, body, choices)
+
+
+def parse_ending(response):
+    text = response.split("###")
+    title, body = text[1], text[2]
+
+    title = title.strip().split(": ")
+    title[0] = int(title[0][8:10])
+    body = body.strip()
+
+    return (title, body)
+
+
+class Response():
+    def __init__(self, response):
+        self.text = self.parse(response.text)
+        self.title = self.text[0]
+        self.body = self.text[1]
+        self.choices = self.text[2]
+        self.choice1 = self.choices[0]
+        self.choice2 = self.choices[1]
+        self.choice3 = self.choices[2]
+        self.choice4 = self.choices[3]
+
+    def parse(self, response):
+        text = response.split("###")
+        title, body, choices = text[1], text[2], text[4]
+
+        title = title.strip().split(": ")
+        title[0] = int(title[0][8:10])
+        body = body.strip()
+        choices = choices.strip()
+
+        choices = choices.split("\n")
+        for i in range(len(choices)):
+            choices[i] = choice_parsing(choices[i])
+
+        return (title, body, choices)
