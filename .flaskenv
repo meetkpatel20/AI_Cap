a527f29f1851d797a87b4541ea9fdf77e3020456
Adam Lear <adamlear12@gmail.com>
2024-04-29 14:37:18 -0500

    swap google gemini with openai api

--- .flaskenv
+++ .flaskenv
@@ -1,2 +1,1 @@
 FLASK_APP=game.py
-FLASK_DEBUG=1
--- app/model_helpers.py
+++ app/model_helpers.py
@@ -10,8 +10,11 @@ def add_message_to_history(history, role, message):
 def send_message(history, prompt):
     add_message_to_history(history, "user", prompt)
     response = client.chat.completions.create(
-        model="gpt-3.5-turbo",
         messages=history,
+        model="gpt-3.5-turbo",
+        frequency_penalty=0.8,
+        presence_penalty=0.8,
+        temperature=1.5,
     )
     message = response.choices[0].message.content
     add_message_to_history(history, "assistant", message)
